e245e38c4f874ab1a934d4b23fca2481a6ae3992
Marucoge <airfromcountryderry@gmail.com>
2018-02-02 17:25:53 +0900

    最初のコミット。(すでにプロトタイプ完成)

---
+++ Assets/_Project/Scripts/InputIntegrator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+//using UnityEngine.UI;
+//using UnityEditor;
+using UnityEngine;
+using System;
+
+
+namespace IntegratedInput{
+    public class InputIntegrator : MonoBehaviour{
+        [SerializeField] private IntegratedButton IntegratedCircleButton;
+        [SerializeField] private IntegratedButton IntegratedCrossButton;
+
+
+        public static ButtonState CircleButton { get; private set; }
+        public static ButtonState CrossButton { get; private set; }
+
+
+        // フレーム内での処理順についてはまだ考慮していない。すべてのIntegratedButton の処理が終わった直後がベストか。
+        private void Update() {
+            CircleButton = IntegratedCircleButton.Status;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/IntegratedButton.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace IntegratedInput{
+    public class IntegratedButton : MonoBehaviour {
+        public ButtonState Status { get; private set; }
+        public InputType InputType = InputType.AxisInput;
+
+        [SerializeField] private string ButtonAxisName = string.Empty;
+        private bool isGUIButtonJustDown = false;
+
+
+       IntegratedButton() {
+            Status = ButtonState.BeingReleased;
+        }
+
+
+        private void LateUpdate() {
+            UpdateGUIInput();
+            UpdateAxisInput();
+        }
+        
+
+        private void UpdateAxisInput() {
+            if (InputType != InputType.AxisInput) { return; }
+            if (string.IsNullOrEmpty(ButtonAxisName)) { return; }
+
+            // 状態が何であろうと、ButtonUp なら BeingReleased に。
+            if (Input.GetButtonUp(ButtonAxisName)) {
+                Status = ButtonState.BeingReleased;
+            }
+
+            // 前のフレームで JustPressed になり、そのままだったら BeingPressed
+            if (Status == ButtonState.JustPressed) {
+                Status = ButtonState.BeingPressed;
+            }
+
+            // ButtonDown なら、次のチェックまでは JustPressed
+            if (Input.GetButtonDown(ButtonAxisName)) {
+                Status = ButtonState.JustPressed;
+            }
+        }
+
+
+        private void UpdateGUIInput() {
+            if (InputType != InputType.GUIInput) { return; }
+
+            // ButtonUp なら BeingReleased に。これはイベント側でやっている。
+
+            // 前のフレームで JustPressed になり、そのままだったら BeingPressed
+            if (Status == ButtonState.JustPressed) {
+                Status = ButtonState.BeingPressed;
+                isGUIButtonJustDown = false;
+            }
+
+            // ButtonDown なら、次のチェックまでは JustPressed
+            if (isGUIButtonJustDown == true) {
+                Status = ButtonState.JustPressed;
+            }
+        }
+
+
+        // GUIのボタンが押された最初のフレームに呼ばれる。(EventTrigger に登録)
+        public void GUIButtonDown() {
+            isGUIButtonJustDown = true;
+        }
+
+
+        // GUI のボタンから指が離れたときに呼ばれる。(EventTrigger に登録)
+        public void GUIButtonUp() {
+            isGUIButtonJustDown = false;
+            Status = ButtonState.BeingReleased;
+        }
+
+
+        // ポインターがGUI ボタンの範囲外に出た時に呼ばれる (EventTrigger に登録)
+        public void GUIButtonExit() {
+            isGUIButtonJustDown = false;
+            Status = ButtonState.BeingReleased;
+        }
+    }
+}
